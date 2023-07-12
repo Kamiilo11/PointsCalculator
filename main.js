3a0a0e8883eabdb0ff3a73d0147ed2d97c5e6672
camiilo laptop <camieros@live.com>
2023-07-12 00:03:30 -0500

    Finished the hide section part for both the sports bets sub sections, and corrected a few styles and some typos in the HTML file, only missing the points calculation section and the result display and it's corresponding styles

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ const sportsSectionBtn = document.querySelector(".sports-btn");
 const sportsDivSection = document.querySelector(".sports-container");
 const sportsFormSportBtn = document.querySelector("#sports-iib");
 const sportsFormSport = document.querySelector(".sports-iih");
+const sportsBtnSingle = document.querySelector("#sc-button");
+const sportOddsSingleSc1 = document.querySelector(".big-cont-1");
+const sportOddsSingleSc2 = document.querySelector(".big-cont-2");
+const sportOddsSingleSc3 = document.querySelector(".big-cont-3");
+const sportOddsSingleSc4 = document.querySelector(".big-cont-4");
+const sportsBtnCombi = document.querySelector(".sc-title2");
+const sportOddsCombiSc1 = document.querySelector(".bigc-cont-1");
+const sportOddsCombiSc2 = document.querySelector(".bigc-cont-2");
+const sportOddsCombiSc3 = document.querySelector(".bigc-cont-3");
+const sportOddsCombiSc4 = document.querySelector(".bigc-cont-4");
 
 
 const submitField = document.querySelector("#stake");
@@ -21,11 +31,11 @@ const submitBtn = document.querySelector("#submit-btn");
 
 
 
-console.log(casinoSlotInput.checked);
+console.log(casinoSlotInput.value);
 console.log(casinoBlackjackInput.value);
 console.log(casinoRouletteInput.value);
-console.log(submitBtn.value);
-console.log(submitField.value);
+console.log(Number(casinoSlotInput.value) + Number(casinoBlackjackInput.value));
+console.log(`combi ${sportsBtnCombi}`);
 
 //Add event listeners
 
@@ -70,5 +80,45 @@ sportsFormSportBtn.addEventListener("click", () => {
     } else if (sportsFormSport.style.display == "block") {
         sportsFormSport.style.display = "";
         console.log(`section ${sportsFormSport.style.display}`);
+    }
+});
+
+sportsBtnSingle.addEventListener("click", () => {
+    if (sportOddsSingleSc1.style.display == "" || sportOddsCombiSc1.style.display == "grid") {
+        sportOddsSingleSc1.style.display = "grid";
+        sportOddsSingleSc2.style.display = "grid";
+        sportOddsSingleSc3.style.display = "grid";
+        sportOddsSingleSc4.style.display = "grid";
+        console.log(sportOddsSingleSc1.style.display);
+        sportOddsCombiSc1.style.display = "";
+        sportOddsCombiSc2.style.display = "";
+        sportOddsCombiSc3.style.display = "";
+        sportOddsCombiSc4.style.display = "";
+        console.log(sportOddsCombiSc1.style.display);
+    } else if (sportOddsSingleSc1.style.display == "grid") {
+        sportOddsSingleSc1.style.display = "";
+        sportOddsSingleSc2.style.display = "";
+        sportOddsSingleSc3.style.display = "";
+        sportOddsSingleSc4.style.display = "";
+    }
+});
+
+sportsBtnCombi.addEventListener("click", () => {
+    if (sportOddsCombiSc1.style.display == "" || sportOddsSingleSc1.style.display == "grid") {
+        sportOddsCombiSc1.style.display = "grid";
+        sportOddsCombiSc2.style.display = "grid";
+        sportOddsCombiSc3.style.display = "grid";
+        sportOddsCombiSc4.style.display = "grid";
+        console.log(sportOddsCombiSc1.style.display);
+        sportOddsSingleSc1.style.display = "";
+        sportOddsSingleSc2.style.display = "";
+        sportOddsSingleSc3.style.display = "";
+        sportOddsSingleSc4.style.display = "";
+        console.log(sportOddsSingleSc1.style.display);
+    } else if (sportOddsCombiSc1.style.display == "grid") {
+        sportOddsCombiSc1.style.display = "";
+        sportOddsCombiSc2.style.display = "";
+        sportOddsCombiSc3.style.display = "";
+        sportOddsCombiSc4.style.display = "";
     }
 });
